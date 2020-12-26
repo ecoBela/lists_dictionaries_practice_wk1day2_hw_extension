22140776656bf603812c5ee229a2f186c1578611
Bela <belahamid@gmail.com>
2020-12-26 23:38:47 +0000

    completed task 5

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -63,6 +63,8 @@ print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
+smallest_number = min(users["Erik"]["lottery_numbers"])
+print(smallest_number)
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh

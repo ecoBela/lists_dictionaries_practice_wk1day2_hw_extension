c0d8ad32977dd5684d2b57be32037dbd5c9388bc
Bela <belahamid@gmail.com>
2020-12-26 23:54:46 +0000

    completed task 9

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -80,6 +80,7 @@ print(users["Erik"]["lottery_numbers"])
 users["Erik"]["home_town"] = "Edinburgh"
 print(users["Erik"]["home_town"])
 
-
 # 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"]["pets"].append({"name": "Fluffy", "species": "dog"})
+print(users["Erik"]["pets"])
 # 10. Add another person to the users dictionary
